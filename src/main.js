c9fc74764f4552907e77d1af3070876c73250c81
LengXQ <qyourname@163.com>
2019-12-13 11:36:53 +0800

    搭建路由和饿了么组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+// 导入接收 路由
+import router from './router/router.js';
+// 导入ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// 注册ElementUI
+Vue.use(ElementUI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
+  // 挂载 router
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+// 导入 路由
+import VueRouter from 'vue-router';
+//注册 vue-router
+Vue.use(VueRouter);
+// 实例化 
+const router = new VueRouter({
+    routes: [{
+        path: '/',
+        component: 
+    }, ]
+});
+
+// 暴露出去
+export default router
